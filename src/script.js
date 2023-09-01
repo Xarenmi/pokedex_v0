ed11f7494d37f3bbfe6221733ad41019989ad0e4
1106 <sanders.gesture-0c@icloud.com>
2023-08-31 18:27:36 -0600

    minor bug on arrow buttons

--- src/script.js
+++ src/script.js
@@ -91,7 +91,73 @@ function setRegionScreen(selectedRegion) {
         photoPath = './src/img/Pokebola.png';
         healthInfo.innerText = 'POKEDEX';
     }
+
     photoElement.src = photoPath;
+
+    const regionSelect = document.getElementById('region');
+
+
+
+    const changeRegion = () => {
+        let sortedPokemons = undefined;
+        const selectedRegion = regionSelect.value;
+        if (selectedRegion === 'all') {
+            sortedPokemons = Pokemon.pokemons.slice().sort((a, b) => a.id - b.id);
+        } else {
+            const filteredPokemons = Pokemon.pokemons.filter(pokemon => pokemon.region.name === selectedRegion);
+            sortedPokemons = filteredPokemons.slice().sort((a, b) => a.id - b.id);
+        }
+
+        let filteredRegion = Region.regions.filter(region => region.name === selectedRegion);
+
+        const infoscreen = document.getElementById('infoscreen');
+
+        let regionTable = '';
+        if (filteredRegion[0]) {
+            regionTable = `<h2>${selectedRegion}</h2>
+        <p id ='Pokescription'>${filteredRegion[0].description}</p>`;
+        }
+        if (sortedPokemons.length > 0) {
+            regionTable += `
+        <table id="regionTable">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+            </tr>
+            </thead>
+            <tbody>
+            ${sortedPokemons.map(pokemon => `
+                <tr pokemon-name="${pokemon.name}" type="${pokemon.kind}" region="${pokemon.region}">
+                <td>${pokemon.id}</td>
+                <td>${pokemon.name}</td>
+                </tr>
+            `).join('')}
+            </tbody>
+        </table>
+        `;
+        } else {
+            regionTable += '<p class="yellow-text">Aún no almacenamos Pokemon de esa categoría.</p>'
+        }
+
+
+        infoscreen.innerHTML = regionTable;
+        infoscreen.style.cursor = 'pointer';
+
+        const pokemonRows = infoscreen.querySelectorAll('tr[pokemon-name]');
+        pokemonRows.forEach(row => {
+            row.addEventListener('click', function () {
+                const pokemonName = this.getAttribute('pokemon-name');
+                const selectedPokemon = Pokemon.pokemons.find(pokemon => pokemon.name === pokemonName);
+                if (selectedPokemon) {
+                    setPokemonScreen(selectedPokemon);
+                }
+            });
+        });
+    }
+    changeRegion();
+    
+    regionSelect.addEventListener('change', changeRegion);
 }
 
 function regionOptions() {
@@ -113,68 +179,11 @@ function regionOptions() {
     regionSelect.addEventListener('change', function () {
         const typeSelect = document.getElementById('type');
         typeSelect.value = 'all';
-
-        let sortedPokemons = undefined;
         const selectedRegion = regionSelect.value;
-        if (selectedRegion === 'all') {
-            sortedPokemons = Pokemon.pokemons.slice().sort((a, b) => a.id - b.id);
-        } else {
-            const filteredPokemons = Pokemon.pokemons.filter(pokemon => pokemon.region.name === selectedRegion);
-            sortedPokemons = filteredPokemons.slice().sort((a, b) => a.id - b.id);
-        }
 
         setRegionScreen(selectedRegion)
 
-        //console.log(selectedRegion)
-        //console.log(Pokemon.pokemons[0].region.name)
-        //console.log(filteredPokemons)
-
-        const infoscreen = document.getElementById('infoscreen');
-
-        let filteredRegion = Region.regions.filter(region => region.name === selectedRegion);
-
-        let regionTable = '';
-        if (filteredRegion[0]) {
-            regionTable = `<h2>${selectedRegion}</h2>
-            <p id ='Pokescription'>${filteredRegion[0].description}</p>`;
-        }
-        if (sortedPokemons.length > 0) {
-            regionTable += `
-            <table>
-                <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                </tr>
-                </thead>
-                <tbody>
-                ${sortedPokemons.map(pokemon => `
-                    <tr pokemon-name="${pokemon.name}">
-                    <td>${pokemon.id}</td>
-                    <td>${pokemon.name}</td>
-                    </tr>
-                `).join('')}
-                </tbody>
-            </table>
-            `;
-        } else {
-            regionTable += '<p class="yellow-text">Aún no almacenamos Pokemon de esa categoría.</p>'
-        }
-
-
-        infoscreen.innerHTML = regionTable;
-        infoscreen.style.cursor = 'pointer';
-
-        const pokemonRows = infoscreen.querySelectorAll('tr[pokemon-name]');
-        pokemonRows.forEach(row => {
-            row.addEventListener('click', function () {
-                const pokemonName = this.getAttribute('pokemon-name');
-                const selectedPokemon = Pokemon.pokemons.find(pokemon => pokemon.name === pokemonName);
-                if (selectedPokemon) {
-                    setPokemonScreen(selectedPokemon);
-                }
-            });
-        });
+        arrowButtons();
     });
 }
 
@@ -220,7 +229,7 @@ function typeOptions() {
             typeTable = `
             <h2></h2>
             <p id='Pokescription'> </p>
-            <table id="filteredPokemons">
+            <table id="typeTable">
                 <thead>
                     <tr>
                         <th>ID</th>
@@ -229,7 +238,7 @@ function typeOptions() {
                 </thead>
             <tbody>
               ${sortedPokemons.map(pokemon => `
-                <tr pokemon-name="${pokemon.name}">
+                <tr pokemon-name="${pokemon.name}" type="${pokemon.kind}" region="${pokemon.region}">
                   <td>${pokemon.id}</td>
                   <td>${pokemon.name}</td>
                 </tr>
@@ -268,47 +277,92 @@ function typeOptions() {
 function arrowButtons() {
     const prevButton = document.getElementById('dis-prev');
     const nextButton = document.getElementById('dis-next');
+    const regionSelect = document.getElementById('region');
+    const typeSelect = document.getElementById('type');
     const pokeDescription = document.getElementById('Pokescription');
     const infoscreen = document.getElementById('infoscreen');
-    const pokemonRows = infoscreen.querySelectorAll('tr[pokemon-name]');
-    const pokeList = Array.from(pokemonRows).map(row => row.getAttribute('pokemon-name'));
+    let pokemonRows = infoscreen.querySelectorAll('tr[pokemon-name]');
+    let pokeList = Array.from(pokemonRows).map(row => row.getAttribute('pokemon-name'));
     let currentIndex = -1;
 
-    pokemonRows.forEach(row => {
-        row.addEventListener('click', function () {
-            currentIndex = pokeList.indexOf(this.getAttribute('pokemon-name'));
-            const clickedPokemon = Pokemon.pokemons.find(pokemon => pokemon.name === pokeList[currentIndex]);
-            pokemonRows.forEach(row => row.classList.remove('selected'));
-            row.classList.add('selected');
+    const typeTable = document.getElementById('typeTable');
+    const regionTable = document.getElementById('regionTable');
+
+    typeSelect.addEventListener('change', function () {
+        pokemonRows = infoscreen.querySelectorAll(`tr[pokemon-name][type="${typeSelect.value}"]`);
+        pokeList = Array.from(pokemonRows).map(row => row.getAttribute('pokemon-name'));
+        if (pokeList.length > 0) {
+            currentIndex = 0;
+        }
+    });
+
+    if (regionTable === null) {
+        pokemonRows.forEach(row => {
+            row.addEventListener('click', function () {
+                currentIndex = pokeList.indexOf(this.getAttribute('pokemon-name'));
+            });
         });
-    });
 
-    prevButton.addEventListener('click', function () {
-        if (currentIndex > 0) {
-            currentIndex--;
-            const prevPokemon = Pokemon.pokemons.find(pokemon => pokemon.name === pokeList[currentIndex]);
-            setPokemonScreen(prevPokemon);
-            pokeDescription.textContent = prevPokemon.description;
-            pokemonRows.forEach(row => row.classList.remove('selected'));
-            const selectedRow = document.querySelector(`tr[pokemon-name="${prevPokemon.name}"]`);
-            selectedRow.classList.add('selected');
-        }
-    });
+        prevButton.addEventListener('click', function () {
 
-    nextButton.addEventListener('click', function () {
-        if (currentIndex < pokeList.length - 1) {
-            currentIndex++;
-            const nextPokemon = Pokemon.pokemons.find(pokemon => pokemon.name === pokeList[currentIndex]);
-            setPokemonScreen(nextPokemon);
-            pokeDescription.textContent = nextPokemon.description;
-            pokemonRows.forEach(row => row.classList.remove('selected'));
-            const selectedRow = document.querySelector(`tr[pokemon-name="${nextPokemon.name}"]`);
-            selectedRow.classList.add('selected');
+            if (currentIndex === 0) {
+                prevButton.disabled = true;
+            }
+            if (currentIndex > 0) {
+                prevButton.disabled = false;
+                currentIndex--;
+                const prevPokemon = Pokemon.pokemons.find(pokemon => pokemon.name === pokeList[currentIndex]);
+                setPokemonScreen(prevPokemon);
+                pokeDescription.textContent = prevPokemon.description;
+            }
+        });
+
+        nextButton.addEventListener('click', function () {
+            if (currentIndex < pokeList.length - 1) {
+                currentIndex++;
+                const nextPokemon = Pokemon.pokemons.find(pokemon => pokemon.name === pokeList[currentIndex]);
+                setPokemonScreen(nextPokemon);
+                pokeDescription.textContent = nextPokemon.description;
+            }
+        });
+    }
+
+    if (typeTable === null) {
+        console.log(typeTable)
+
+        const possibleValues = Array.from(regionSelect.options);
+        let regionNames = possibleValues.map(region => region.value);
+        const selectedRegion = Region.regions.filter(region => region.name === regionSelect.value);
+
+
+        if (regionSelect.value !== 'all') {
+            currentIndex = regionNames.indexOf(selectedRegion[0].name);
         }
-    });
+
+
+
+        prevButton.addEventListener('click', function () {
+            if (currentIndex > 1) {
+                currentIndex--;
+                regionSelect.value = regionNames[currentIndex];
+                setRegionScreen(regionSelect.value);
+                console.log(currentIndex, regionNames[currentIndex]);
+            }
+        });
+
+        nextButton.addEventListener('click', function () {
+            if (currentIndex < regionNames.length - 1) {
+                currentIndex++;
+                regionSelect.value = regionNames[currentIndex];
+                setRegionScreen(regionSelect.value);
+                console.log(currentIndex, regionNames[currentIndex]);
+            }
+        });
+    }
 }
 
 regionOptions();
 typeOptions();
+
 // Se repiten elementos en diferentes funciones para que no se puedan editar fuera de la funcion.
 
